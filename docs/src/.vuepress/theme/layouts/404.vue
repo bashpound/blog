a468f57462e0b30d209688486ff320c2a0790918
bashpound <62741762+bashpound@users.noreply.github.com>
2020-11-04 18:00:42 +0900

    404

--- docs/src/.vuepress/theme/layouts/404.vue
+++ docs/src/.vuepress/theme/layouts/404.vue
@@ -1,30 +1,59 @@
 <template>
   <div class="theme-container">
-    <div class="theme-default-content">
-      <h1>404</h1>
+    <div class="theme-default-content centered">
+      <h1>404 page not found</h1>
 
       <blockquote>{{ getMsg() }}</blockquote>
 
+      
+
       <RouterLink to="/">
-        Take me home.
+        <h3>Take me home</h3>
       </RouterLink>
+      <br />
+      <h3>Redirecting in {{counter}}s....</h3>
     </div>
   </div>
 </template>
 
 <script>
 const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
   `Looks like we've got some broken links.`
 ]
 
 export default {
+  data () {
+    return {
+      counter: 3
+    }
+  },
+  mounted() {
+    this.countDown()
+  },
   methods: {
     getMsg () {
       return msgs[Math.floor(Math.random() * msgs.length)]
+    },
+    countDown() {
+      if(this.counter >0) {
+        setTimeout(()=>{
+          this.counter--
+          this.countDown()
+        }, 1000)
+      }
+      if(this.counter === 0 ){
+        window.location.href="http://bashpound.github.io"
+      }
     }
   }
 }
 </script>
+
+
+<style scoped>
+
+.centered  {
+  margin: auto;
+}
+
+</style>
